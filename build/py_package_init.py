2e4540937f2c1d66b0921a9eccbd75cbd6debe69
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-05-10 07:19:39 -0500

    Integrate code gen into PIO pre-build

---
+++ build/py_package_init.py
@@ -0,0 +1,20 @@
+import sys
+import subprocess
+import pkg_resources
+import site
+
+python = sys.executable
+package_path = site.getsitepackages()
+
+installed = [ pkg.key for pkg in pkg_resources.working_set ]
+
+subprocess.check_call([python, '--version'])
+subprocess.check_call([python, '-m', 'pip', '--version'])
+
+required_packages = ["more-itertools"]
+for package in required_packages:
+    if package in installed:
+        print(f'Package {package} already installed.')
+    else:
+        print(f"Installing: {package}")
+        subprocess.check_call([python, '-m', 'pip', 'install', package])
